cdcf4ffdf5b8ef93676fbf7edcfe9ba1156c3dfb
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-10-19 18:08:54 +0600

    toggle

---
+++ index.js
@@ -0,0 +1,9 @@
+const function1 = function () {
+    // return 2
+    console.log("//////////")
+}
+const function2 = function () {
+    // return 2
+    console.log(".........");
+}
+console.log(function1 == function2);
